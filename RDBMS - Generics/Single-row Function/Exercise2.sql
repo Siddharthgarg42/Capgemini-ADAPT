f156d12ef944d4a9d7ef36eea48e3cdcb08b2035
Siddharthgarg42 <51908972+Siddharthgarg42@users.noreply.github.com>
2021-02-17 17:50:37 +0530

    Update Exercise2.sql

--- RDBMS - Generics/Single-row Function/Exercise2.sql
+++ RDBMS - Generics/Single-row Function/Exercise2.sql
@@ -2,3 +2,5 @@
 
 alter session set current_schema=bank;
 SET heading OFF;
+select TRANS_DT , substr(TRANS_DESC ,1,20) as TRANS_DESC ,TRANS_AMT from BANK_TRANSACTION  
+where TO_CHAR(TRANS_DT,'Mon-YYYY')='Jun-2010' ;
